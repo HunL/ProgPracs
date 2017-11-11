ca12a286fe010f9324f42979b93ebd08193d6472
HunL <ljiehui0826@gmail.com>
2017-11-11 16:39:19 +0800

    Print some dict examples

--- PY/print_list.py
+++ PY/print_list.py
@@ -1,11 +1,39 @@
-print '>>>>>print directly>>>>>'
+print '>>>>> print directly >>>>>'
 print range(0,13)
 
-print '>>>>>print list>>>>>'
+print '>>>>> list >>>>>'
 print [x * x for x in range(1,8)]
 
-print '>>>>>print condition filter>>>>>'
+print '>>>>> condition filter >>>>>'
 print [x * x for x in range(1,11) if x % 2 != 0]
 
-print '>>>>>print multiple nested'
+print '>>>>> multiple nested >>>>>'
 print [m + n for m in 'abc' for n in '123']
+
+print '>>>>> enumerate >>>>>'
+L = ['Adam', 'Lisa', 'Bart', 'Paul']
+for index,value in enumerate(L):
+    print index,'-',value
+
+
+
+print '>>>>> iteration of dict >>>>>'
+d = {'Adam':95, 'Lisa':80, 'Bart':60}
+
+print '>>>>> directly >>>>>'
+print d.values()
+
+print '>>>>> normal iteration >>>>>'
+for v in d.values():
+    print v
+
+print '>>>>> iteration for save memory >>>>>'
+for v in d.itervalues():
+    print v 
+
+print '>>>>> k-v >>>>>'
+print d.items()
+
+print '>>>>> k-v iteration >>>>>'
+for k,v in d.iteritems():
+    print k,':',v
