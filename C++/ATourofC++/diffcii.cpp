1ec1d40385e16e4c78ddfc6d5bf84fd93ae904c7
lijiehui <ljiehui0826@gmail.com>
2018-07-09 09:31:14 +0800

    g++ -std=c++11 diffcii.cpp

--- C++/ATourofC++/diffcii.cpp
+++ C++/ATourofC++/diffcii.cpp
@@ -6,11 +6,11 @@ void fore_inc() {
     int v1[10] = {0,1,2,3,4,5,6,7,8,9};
     int v2[10];
 
-    for (auto int i=0;i!=10;++i)
+    for (auto i=0;i!=10;++i)
         v2[i]=v1[i];
 
-    for (auto int i=0;i!=10;++i)
-        cout << "v1: " << v1[i] << ", v2: " << v2[i] << endl;
+    for (auto i=0;i!=10;++i)
+        cout << "fore v1: " << v1[i] << ", v2: " << v2[i] << endl;
 
     cout << endl;
 }
@@ -19,16 +19,16 @@ void back_inc() {
     int v1[10] = {0,1,2,3,4,5,6,7,8,9};
     int v2[10];
 
-    for (auto int i=0;i!=10;i++)
+    for (auto i=0;i!=10;i++)
         v2[i]=v1[i];
 
-    for (auto int i=0;i!=10;i++)
+    for (auto i=0;i!=10;i++)
         cout << "back v1: " << v1[i] << ", v2: " << v2[i] << endl;
 
     cout << endl;
 }
 
 int main() {
-    //fore_inc();
-    back_inc();
+    fore_inc();
+    //back_inc();
 }
