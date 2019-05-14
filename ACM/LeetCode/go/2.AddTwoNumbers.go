d43d8dc10f33374bde63a2eda5d54742910d8544
HunL <843101979@qq.com>
2019-05-14 08:38:57 +0800

    do more further about 2.AddTwoNumbers.go: save num in ListNode in reverse order

--- ACM/LeetCode/go/2.AddTwoNumbers.go
+++ ACM/LeetCode/go/2.AddTwoNumbers.go
@@ -10,14 +10,33 @@ type ListNode struct {
 }
 
 func main() {
-    l1 := getListNode(123)
-    showNodes(l1)
+    // test(12)
+    // showNodes(test1)
 
-    l2 := getListNode(345)
-    showNodes(l2)
+    test1 := getListNode1(123)
+    showNodes(test1)
 
-    l3 := addTwoNumbers(l1, l2)
-    showNodes(l3)
+    test2 := getListNode1(345)
+    showNodes(test2)
+
+    test3 := addTwoNumbers(test1, test2)
+    showNodes(test3)
+
+    // l1 := getListNode(123)
+    // showNodes(l1)
+
+    // l2 := getListNode(345)
+    // showNodes(l2)
+
+    // l3 := addTwoNumbers(l1, l2)
+    // showNodes(l3)
+}
+func test(num int) {
+    var val = num % 10
+    Print("test val: ", val)
+
+    var val2 = num / 10
+    Print("\t test val2: ", val2, "\n")
 }
 
 func getListNode(num int) *ListNode {
@@ -33,6 +52,42 @@ func getListNode(num int) *ListNode {
     return head
 }
 
+func getListNode1(num int) *ListNode {
+    Print("\norigin num: ", num)
+    var head = new(ListNode)
+    if (num == 0) {
+        head.Val = 0
+        return head
+    }
+    head.Val = num % 10
+    num = num / 10
+    // num = reverse_num(num)
+    for (num != 0) {
+        Print("\nnum: ", num, "\t")
+        var node = new(ListNode)
+        node.Val = num % 10
+        Print("\nnode.Val: ", node.Val, "\t")
+        num = num / 10
+        node.Next = head.Next
+        head.Next = node
+    }
+    Println()
+    return head
+}
+
+func reverse_num(num int) int {
+    if (num == 0) {
+        return num
+    }
+    
+    for (num != 0) {
+        // a := num % 10
+        // b := num / 10
+
+    }
+    return 0
+}
+
 func showNodes(p *ListNode) {
     for (p != nil) {
         Print("show: ", p.Val, "\t")
@@ -43,6 +98,9 @@ func showNodes(p *ListNode) {
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
     var l3 = new(ListNode)
+    l3.Val = l1.Val+l2.Val
+    l1 = l1.Next
+    l2 = l2.Next
     
     for (l1 != nil) {
         Print("add: ", l1.Val, "\t", l2.Val)
